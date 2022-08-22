e21db63a26e96b4ae0c285bf74570e16c0c16b45
sanakdam <san.akdam@gmail.com>
2022-08-22 20:15:38 +0700

    Assignment 2

---
+++ Assignment_2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := []string{
+		"Teguh Afdilla",
+		"Fajar Agus Maulana",
+		"Kadek Bintang A",
+		"David B",
+		"Stevanus Dewana",
+		"Fisaaa",
+		"Iqbal Hamdani",
+		"Kevin Hugo",
+		"Irfan Maulana",
+		"Edwin Noegroho",
+	}
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
